f61a56d345ae608f90fc6c5f66b5d478d6471d25
cudderson <codered1227@gmail.com>
2020-12-26 19:15:13 -0800

    Added functions for modifying/deleting data in file. Ready to merge to master.

--- password_manager.py
+++ password_manager.py
@@ -27,6 +27,9 @@ with open("encryption_key.txt", "rb") as f:
 
 fk = Fernet(crypt_key)
 
+# All current functionality working.
+# Next, work on dialogue smoothness/readability, and patch holes in the dialogue
+
 def AddPassword():
     new_id = input("New ID (ex. 'Twitter'): ")
     new_pass = input("New Password for " + new_id + ": ")
@@ -49,15 +52,78 @@ def ViewPasswords():
             print(decrypted.decode())
             line = f.readline()
 
-# current state: viewing and adding work. Next, it would be good to have functionality for
-# changing a password, or deleting one.
+def ModifyPassword():
+    temp_list = []
+    with open("my_passwords.txt", "r") as f:
+        line = f.readline()
+        while line != "":
+            line = line.encode()
+            decrypted = fk.decrypt(line)
+            decrypted = decrypted.decode()
+            print(decrypted)
+            temp_list.append(decrypted)
+            line = f.readline()
+    print(temp_list)
+    id_to_mod = input("Please input the ID you would like to modify/remove (ex. 'Playstation': ")
+    for data in temp_list:
+        z = data.find(id_to_mod)
+        if z == -1:
+            pass
+        else:
+            print(data)
+            print("Would you like to modify your password for " + id_to_mod + ", or remove it from record?: ")
+            mod_or_rem = input("Type 'm' to modify\n"
+                               "Type 'r' to remove\n"
+                               "Type 'q' to quit: ")
+            if mod_or_rem == 'r': # this 'if' works as intended!
+                temp_list.remove(data)
+                with open("my_passwords.txt", "w") as f:
+                    for plain_data in temp_list:
+                        plain_data = plain_data.encode("UTF-8")
+                        e_data = fk.encrypt(plain_data)
+                        e_data = e_data.decode()  # normal string now (encrypted)
+                        f.write(e_data)
+                        f.write("\n")
+                    print("DONEZO")
+            elif mod_or_rem == 'm':
+                pass_to_mod = input("Enter your current password for " + id_to_mod + ": ")
+                new_pass = input("Enter your new password for " + id_to_mod + ": ")
+                print("Ready to change password for " + id_to_mod + " from " + pass_to_mod + " to " + new_pass + ",")
+                confirm_new_pass = input("Type 'confirm' to apply change, or type 'q' to quit: ")
+                if confirm_new_pass == 'confirm': #this entire 'if' works as intended
+                    temp_list.remove(data)
+                    temp_list.append(data.replace(pass_to_mod, new_pass))
+                    print(temp_list)
+                    print("^^^new data")
+                    # works up to here. just need to write data back into file
+                    with open("my_passwords.txt", "w") as f:
+                        for plain_data in temp_list:
+                            plain_data = plain_data.encode("UTF-8")
+                            e_data = fk.encrypt(plain_data)
+                            e_data = e_data.decode() #normal string now (encrypted)
+                            f.write(e_data)
+                            f.write("\n")
+                    print("DONE")
+                    break
+                else:
+                    print("Operation cancelled. Nothing was altered.")
+                    quit()
+                # tell user to enter password for id to change. ask user what new password should be.
+                # replace old password with new password, then overwrite new data to file (later)
+            else:
+                print("Operation cancelled. Nothing was altered.")
+                quit()
+
 while True:
     mode = input("Welcome to Password Manager!\n"
           "Type 'v' to view your passwords\n"
-          "Type 'a' to add a new password: ")
+          "Type 'a' to add a new password\n"
+          "Type 'm' to modify passwords: ")
     if mode == 'v':
         ViewPasswords()
     elif mode == 'a':
         AddPassword()
+    elif mode == 'm':
+        ModifyPassword()
     else:
         break
