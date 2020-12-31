48bf9cb3ad497b4013df3c1784408f85931e7e75
cudderson <codered1227@gmail.com>
2020-12-30 16:27:06 -0800

    still optimizing dialogue

--- password_manager.py
+++ password_manager.py
@@ -44,7 +44,6 @@ def AddPassword():
         f.write("\n")
         print("\nSuccessfully saved data for ID: " + new_id + " with password: " + new_pass)
 
-
 def ViewPasswords():
     with open("my_passwords.txt", "r") as f:
         line = f.readline()
@@ -55,6 +54,7 @@ def ViewPasswords():
             line = f.readline()
 
 def ModifyPassword():
+    print("\nMake sure to type ID/Password identical to how it is provided. Otherwise, changes may not commit!\n")
     temp_list = []
     with open("my_passwords.txt", "r") as f:
         line = f.readline()
@@ -65,19 +65,18 @@ def ModifyPassword():
             print(decrypted)
             temp_list.append(decrypted)
             line = f.readline()
-    print(temp_list)
-    id_to_mod = input("Please input the ID you would like to modify/remove (ex. 'Playstation': ")
-    for data in temp_list:
+    id_to_mod = input("\nPlease input the ID or Password  you would like to modify/remove (ex. 'Playstation': ")
+    for data in temp_list: # needs work
         z = data.find(id_to_mod)
         if z == -1:
             pass
         else:
-            print(data)
-            print("Would you like to modify your password for " + id_to_mod + ", or remove it from record?: ")
+            print("\n" + data)
+            print("\nWould you like to modify your password for " + id_to_mod + ", or remove it from record?: ")
             mod_or_rem = input("Type 'm' to modify\n"
                                "Type 'r' to remove\n"
                                "Type 'q' to quit: ")
-            if mod_or_rem == 'r':  # this 'if' works as intended!
+            if mod_or_rem == 'r':
                 temp_list.remove(data)
                 with open("my_passwords.txt", "w") as f:
                     for plain_data in temp_list:
@@ -86,7 +85,7 @@ def ModifyPassword():
                         e_data = e_data.decode()
                         f.write(e_data)
                         f.write("\n")
-                    print("DONEZO")
+                    print("\nSuccessfully deleted data for " + id_to_mod + ".")
             elif mod_or_rem == 'm':
                 pass_to_mod = input("Enter your current password for " + id_to_mod + ": ")
                 new_pass = input("Enter your new password for " + id_to_mod + ": ")
@@ -95,8 +94,8 @@ def ModifyPassword():
                 if confirm_new_pass == 'confirm':  # this entire 'if' works as intended
                     temp_list.remove(data)
                     temp_list.append(data.replace(pass_to_mod, new_pass))
+                    print("New data:")
                     print(temp_list)
-                    print("^^^new data")
                     with open("my_passwords.txt", "w") as f:
                         for plain_data in temp_list:
                             plain_data = plain_data.encode("UTF-8")
@@ -104,15 +103,14 @@ def ModifyPassword():
                             e_data = e_data.decode()
                             f.write(e_data)
                             f.write("\n")
-                    print("DONE")
+                    print("\nNew data successfully saved.")
                     break
                 else:
-                    print("Operation cancelled. Nothing was altered.")
+                    print("\nOperation cancelled. Nothing was altered.")
                     quit()
             else:
-                print("Operation cancelled. Nothing was altered.")
+                print("\nOperation cancelled. Nothing was altered.")
                 quit()
-
 
 while True:
     mode = input("\nWelcome to Password Manager!\n"
@@ -138,4 +136,4 @@ while True:
 # idea: initialize program start with user providing pin number (allows access to decrypting info from file)
 #       -pin number stored in file similar to the other text files
 #       -if pin is incorrect, program quits.
-# if you do this, make sure to update the .gitignore file
+#       -if you do this, make sure to update the .gitignore file
