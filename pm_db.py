5c6b9c9d3361ed3ad65d2d09801eb1a3ec727268
cudderson <codered1227@gmail.com>
2021-01-14 19:24:35 -0800

    start big refactor with SQL

--- pm_db.py
+++ pm_db.py
@@ -8,16 +8,17 @@ pw_db = mysql.connector.connect(
     database='pw_db'
 )
 
+
+# TASK: set up table structure for project
+#   - one table holding user info, one holding (encrypted) passwords. attached by primary key 'entry'
+# TASK: successfully read/write to database
+# TASK: successfully read/write to database with fernet
+
 # cursor for interacting with database
 my_cursor = pw_db.cursor()
 
 # pw_db.commit() <-- must run this line to commit changes to database
 # my_cursor.execute("CREATE TABLE users (entry int AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50), site VARCHAR(50))")
-
-my_cursor.execute("INSERT INTO users (username, site) VALUES (%s,%s)", ("Lucy", "Youtube"))
-
-my_cursor.execute("SELECT * FROM users")
-for x in my_cursor:
-    print(x)
-
-# pw_db.commit()
+# my_cursor.execute("INSERT INTO users (username, site) VALUES (%s,%s)", ("Lucy", "Youtube"))
+# my_cursor.execute("CREATE TABLE Passwords (entry int AUTO_INCREMENT PRIMARY KEY, password VARCHAR(50))")
+pw_db.commit()
